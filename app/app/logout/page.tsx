f87b1f8922a7b801f7a4cac957c6757955db9b47
sgmikami03 <sgmikami03@gmail.com>
2023-11-27 00:04:05 +0900

    [add]ログアウト 画面作成

--- app/app/logout/page.tsx
+++ app/app/logout/page.tsx
@@ -4,12 +4,16 @@ import { FormEvent, useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
 import type { Database } from "@/lib/database.types";
+import Contents from "@/components/auth/contents";
+import Link from "next/link";
+import { Button, useToast } from "@chakra-ui/react";
 
 // サインアップ
 const Logout = () => {
   const router = useRouter();
   const supabase = createClientComponentClient<Database>();
   const [loading, setLoading] = useState(false);
+  const toast = useToast();
   const [message, setMessage] = useState("");
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
@@ -22,34 +26,56 @@ const Logout = () => {
 
       // エラーチェック
       if (error) {
-        setMessage("エラーが発生しました。" + error.message);
+        toast({
+          title: "エラーが発生しました。",
+          description: error.message,
+          status: "error",
+          duration: 10000,
+          isClosable: true,
+        });
         return;
       }
 
       router.push("/");
     } catch (error) {
-      setMessage("エラーが発生しました。" + error);
+      toast({
+        title: "エラーが発生しました。",
+        description: String(error),
+        status: "error",
+        duration: 10000,
+        isClosable: true,
+      });
       return;
     } finally {
+      toast({
+        title: "ログアウト完了",
+        description: "ログアウトが完了しました。",
+        status: "success",
+        duration: 10000,
+        isClosable: true,
+      });
       setLoading(false);
       router.refresh();
     }
   };
 
   return (
-    <div>
-      <h1>ログアウト</h1>
+    <Contents>
+      <h1 className="mb-[20px] text-3xl font-bold tracking-wide">Logout</h1>
       <form onSubmit={onSubmit}>
-        <div>
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <button type="submit">ログアウト</button>
-          )}
+        <div className="mb-[20px]">
+          <Button type="submit" colorScheme="red" isDisabled={loading}>
+            ログアウト
+          </Button>
         </div>
-        {message && <p>{message}</p>}
+        <hr className="mb-[20px] color-[#E2E8F0]" />
+        <div className="flex justify-center gap-[20px]">
+          <Link href="/" className="text-app-main underline font-medium">
+            ログアウトせずに利用する
+          </Link>
+        </div>
       </form>
-    </div>
+    </Contents>
   );
 };
 
