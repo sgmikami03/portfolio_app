088d1a76cd11609e7d46c7d78dd11eeb07ab9de1
sgmikami03 <sgmikami03@gmail.com>
2023-11-26 14:59:11 +0900

    [add]supabase認証 ログイン追加

---
+++ app/app/login/layout.tsx
@@ -0,0 +1,22 @@
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
+import type { Database } from "@/lib/database.types";
+
+const Signup = async ({ children }: { children: React.ReactNode }) => {
+  //ログインしているかどうか監視して、リダイレクト
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/");
+  }
+
+  return <div>{children}</div>;
+};
+
+export default Signup;
