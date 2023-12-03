38014f6b695e39aca0cd49ef22d00510a2fc14ce
sgmikami03 <sgmikami03@gmail.com>
2023-12-03 21:09:03 +0900

    [add] profile type 追加

---
+++ app/type/index.ts
@@ -0,0 +1,8 @@
+export type Profile ={
+    id: string; //uuid
+    email?: string; //メールアドレス 取得しなくてもいい
+    name: string; //表示名
+    introduce?: string; //自己紹介テキスト
+    icon_image?: string; //アイコン画像パス
+    cover_image?: string; //カバー画像パス
+}
