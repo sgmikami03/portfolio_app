4125766c241c1ed042a317a2d83f51c366949f66
sgmikami03 <sgmikami03@gmail.com>
2023-12-04 09:08:53 +0900

    [add]career table 追加に伴う修正

--- app/lib/supabase/profiles.ts
+++ app/lib/supabase/profiles.ts
@@ -1,6 +1,19 @@
 import supabase from "@/utils/supabase";
 
 export const getProfileById: any | null = async (id: string) => {
-    const {data: profile } = await supabase.from("profiles").select().eq("id", id).single();
+    const { data: profile } = await supabase
+      .from("profiles")
+      .select()
+      .eq("id", id)
+      .single();
     return profile;
+  };
+
+export const getProfileByIdWithCareer: any | null = async (id: string) => {
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select(`id, name, occupation, careers(id, name, text, occupation, start, end)`)
+    .eq("id", id)
+    .single();
+  return profile;
 };
--- app/type/index.ts
+++ app/type/index.ts
@@ -1,9 +1,18 @@
-export type Profile ={
-    id: string; //uuid
-    email?: string; //メールアドレス 取得しなくてもいい
-    name: string; //表示名
-    introduce?: string; //自己紹介テキスト
-    icon_image?: string; //アイコン画像パス
-    cover_image?: string; //カバー画像パス
-    occupation?: string; //職種
-}
+export type Profile = {
+  id: string; //uuid
+  email?: string; //メールアドレス 取得しなくてもいい
+  name: string; //表示名
+  introduce?: string; //自己紹介テキスト
+  icon_image?: string; //アイコン画像パス
+  cover_image?: string; //カバー画像パス
+  occupation?: string; //職種
+};
+
+export type Careers = {
+	id: string; //uuid
+	name: string; //会社名
+	text?: string; //やったこと
+	occupation?: string; //職種
+	start?: Date; //開始日
+	end: Date; //終了日
+};
