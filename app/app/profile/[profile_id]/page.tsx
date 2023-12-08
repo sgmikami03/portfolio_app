ffd88af0f88e8d62969656fdbdff9c91cde339d6
sgmikami03 <sgmikami03@gmail.com>
2023-12-09 00:08:15 +0900

    [add] 認証情報取得 isEdit追加

--- app/app/profile/[profile_id]/page.tsx
+++ app/app/profile/[profile_id]/page.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from "next";
+import { cookies } from "next/headers";
 import { getProfileByIdWithCareer } from "@/lib/supabase/profiles";
 import { Profile } from "@/type";
 import ProfileHeader from "@/components/profile/ProfileHeader";
 import ProfileTab from "@/components/profile/ProfileTab";
 import ProfileCareers from "@/components/profile/ProfileCareers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 type PageProps = {
   params: {
@@ -15,11 +17,20 @@ const ProfilePage: NextPage<PageProps> = async ({ params }) => {
   const profile_id: string = params.profile_id;
   const profile: Profile | null = await getProfileByIdWithCareer(profile_id);
 
+  //ログインユーザーの取得
+  const supabase = createServerComponentClient({
+    cookies,
+  });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const isEdit = session?.user.id == profile?.id;
+
   return (
     <>
-      <ProfileHeader profile={profile} />
+      <ProfileHeader profile={profile} isEdit={isEdit} />
       <ProfileTab profileId={profile_id} tabNum={0} />
-      <ProfileCareers careers={profile?.careers ?? []} />
+      <ProfileCareers careers={profile?.careers ?? []} isEdit={isEdit} />
     </>
   );
 };
